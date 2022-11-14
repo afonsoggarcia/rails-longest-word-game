d817bbd6c77696be74776548bd71c734125f7557
Afonso Garcia <afonso.guerra.garcia@gmail.com>
2022-11-14 18:29:05 -0300

    Game created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,26 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = 10.times.map { ('a'..'z').to_a[rand(26)] }.join(' ')
+    @letters = ('a'..'z').to_a.sample(10)
   end
 
   def score
-  
+    @word = params[:word].split("")
+    @letters = params[:letters]
+    array = []
+    @word.each do |letter|
+      array << @letters.slice!(letter)
+    end
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    response = URI.open(url).read
+    json = JSON.parse(response)
+    check = json['found']
+    if array == @word
+      check == true ? @result = 'Word is valid' : @result = 'Word is not valid'
+    else
+      @result = 'Sorry'
+    end
   end
 end
